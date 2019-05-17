339d947db48dcad8805423b04af3dad26263a17b
Sergey Hursevich <hursik94@gmail.com>
2019-05-17 15:30:33 +0300

    add: finally task

--- src/script/script.js
+++ src/script/script.js
@@ -2,6 +2,7 @@ window.onload = function () {
   const btn = document.querySelector('img');
   const menu = document.querySelector('menu');
   const close = document.querySelector('.menu__awesome');
+  let flag = false;
 
   btn.addEventListener('click', () => {
     menu.style.display = 'block';
@@ -13,5 +14,18 @@ window.onload = function () {
     menu.style.display = 'none';
     document.body.style.overflowY = 'scroll';
     close.style.display = 'none';
+  });
+
+  window.addEventListener('resize', () => {
+    const width = document.body.clientWidth;
+    console.log(width);
+
+    if (width > 769) {
+    menu.style.display = 'block';
+    } else {
+      menu.style.display = 'none';
+    }
+    
+
   });
 }
